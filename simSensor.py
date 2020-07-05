45b1ee82e87279145a397f053a0a5cb1b810feb8
deriando <deriandogoh@gmail.com>
2020-07-06 00:32:21 +0800

    removed class usage

--- simSensor.py
+++ simSensor.py
@@ -2,43 +2,41 @@ import random
 from random import randint
 import time
 
-class simSensor:
-    def DHT22():
-        dht22temp = round(random.uniform(-40, 80), 2)
-        dht22humid = round(random.uniform(0, 1), 2)
-        #time.sleep(2) # real DHT22 sensor only polls once every 2 seconds ***disabled for testing***
-        return dht22temp, dht22humid
+def DHT22():
+    dht22temp = round(random.uniform(-40, 80), 2)
+    dht22humid = round(random.uniform(0, 1), 2)
+    # time.sleep(2) # real DHT22 sensor only polls once every 2 seconds ***disabled for testing***
+    return dht22temp, dht22humid
 
-    def light():
-        lightLevel = round(random.uniform(0, 1), 12)
-        #time.sleep(2) # ***disabled for testing***
-        return lightLevel
-    
-    def moisture():
-        mlvl = randint(260,520)
-        if(mlvl<=440 and mlvl>360):
-            moisture = "water"
-        elif (mlvl<=520 and mlvl>440):
-            moisture = "wet"
-        else:
-            moisture = "dry"
-        #time.sleep(2)
+def light():
+    lightLevel = round(random.uniform(0, 1), 12)
+    # time.sleep(2) # ***disabled for testing***
+    return lightLevel
 
-        return moisture
+def moisture():
+    mlvl = randint(260, 520)
+    if(mlvl <=440 and mlvl>360):
+        moisture = "water"
+    elif (mlvl <=520 and mlvl>440):
+        moisture = "wet"
+    else:
+        moisture = "dry"
+    # time.sleep(2)
+    return moisture
 
-    def CO2():
-        co2 = round(random.uniform(50, 100), 2)
-        #time.sleep(2)
-        return co2
+def CO2():
+    co2 = round(random.uniform(50, 100), 2)
+    # time.sleep(2)
+    return co2
 
 if __name__ == "__main__":
     try:
         while True:
-            print("Light = " + str(simSensor.light())) # high = dark, low = bright
-            print("DHTtemp = " + str(simSensor.DHT22()[0])) # data received is in tuple, use [] to specify which value
-            print("DHThumid = " + str(simSensor.DHT22()[1]))
-            print("Moisture = " + str(simSensor.moisture()))
-            print("CO2 = " + str(simSensor.CO2()))
+            print("Light = " + str(light()))  # high = dark, low = bright
+            print("DHTtemp = " + str(DHT22()[0]))  # data received is in tuple, use [] to specify which value
+            print("DHThumid = " + str(DHT22()[1]))
+            print("Moisture = " + str(moisture()))
+            print("CO2 = " + str(CO2()))
             print("\n")
             time.sleep(2)
     except Exception as e:
