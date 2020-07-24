3b0709f8914b02094735f3ec943bccafce04ea32
JC1005 <joshuachua.18@ichat.sp.edu.sg>
2020-07-24 16:18:58 +0800

    Updated dashboard
    It can display the data real time but unable to display the current time in the x axis of the graph

--- dashboard.py
+++ dashboard.py
@@ -3,59 +3,76 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-import datetime
 from random import randint
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly
 import plotly.graph_objs as go
 from collections import deque
+from datetime import datetime
+import pandas as pd
+import time
+import random
 
-
-X = deque(maxlen=20)
-X.append(1)
-Y = deque(maxlen=20)
-Y.append(1)
 
 app = dash.Dash(__name__)
 
+app_color = {"graph_bg": "#082255", "graph_line": "#007ACE"}
+
+mlvl =[]
+times = []
+
+max_length = 10
+
+times = deque(maxlen=max_length)
+mlvl = deque(maxlen=max_length)
+
 app.layout = html.Div(
     html.Div([
-        dcc.Graph(id='live-graph', animate=True),
+        dcc.Graph(id='live-graph'),
         dcc.Interval(
             id='interval-component',
-            interval=10*1000 # in milliseconds
+            interval=10*1000, # in milliseconds
+            n_intervals=0
         )
     ])
 )
 
 
+def update_obd_values():
+
+    now = datetime.now()
+    current_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    #current_time = now.strftime("%H:%M:%S")
+    times.append(current_time)
+    mlvl.append(random.randrange(260,520))
+
+    return times, mlvl
+
+
 # Multiple components can update everytime interval gets fired.
 @app.callback(Output('live-graph', 'figure'),
-              [Input('interval-component', 'interval')])
-def update_graph_scatter(n):
+              [Input('interval-component', 'n_intervals')])
+def update_graph_scatter(interval):
 
-    time = datetime.datetime.now()
-    mlvl = randint(260,520)
-    Y.append(mlvl)
-    X.append(time)
+    update_obd_values()
+    print(times)
+    data = go.Scatter(
+    x=list(times),
+    y=list(mlvl),
+    name='Scatter',
+    fill="tozeroy",
+    fillcolor="#6897bb"
+    )
 
-    # Create the graph with subplots
-    data = plotly.graph_objs.Scatter(
-            x=list(X),
-            y=list(Y),
-            name='Scatter',
-            mode= 'lines+markers'
-            )
+    layout = go.Layout(xaxis=dict(range=[min(times),max(times)]),
+                                                    yaxis=dict(range=[min(mlvl),max(mlvl)]),
+                                                    margin={'l':50,'r':1,'t':45,'b':1})
     
-    layout = plotly.graph_objs.Layout(xaxis={'type': 'date', 
-                                         'tick0': x[0], 
-                                         'tickmode': 'linear', 
-                                         'dtick': 86400000.0 * 14}, yaxis=dict(range=[260,520]))
+    return dict(data=[data], layout = layout)
 
-    return {'data': [data],'layout' : [layout] }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
